ff212ae89959239b55602df714371b152dad8127
Mert Melih Aytemur <melih.aytemur@appcent.mobi>
2024-04-21 13:54:19 +0300

    add extensions

---
+++ app/src/main/java/tr/com/mermela/common/extension/ContextExtensions.kt
@@ -0,0 +1,8 @@
+package tr.com.mermela.common.extension
+
+import android.content.Context
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+
+fun Context.getDrawableCompat(@DrawableRes resId: Int) =
+    ContextCompat.getDrawable(this, resId)
