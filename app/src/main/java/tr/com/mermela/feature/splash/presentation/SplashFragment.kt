eca4a8fee63d0b419987eabd0cf82b1088fcab3e
Mert Melih Aytemur <melih.aytemur@appcent.mobi>
2024-04-21 14:09:12 +0300

    add splash screen design

--- app/src/main/java/tr/com/mermela/feature/splash/presentation/SplashFragment.kt
+++ app/src/main/java/tr/com/mermela/feature/splash/presentation/SplashFragment.kt
@@ -1,20 +1,34 @@
 package tr.com.mermela.feature.splash.presentation
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import tr.com.mermela.R
+import tr.com.mermela.databinding.FragmentSplashBinding
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
+
+    private lateinit var binding : FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initSplash()
     }
     private fun initSplash() {
@@ -25,10 +39,6 @@ class SplashFragment : Fragment() {
     }
 
     companion object {
-
         private const val DELAY_SPLASH = 2000L
-
-        fun newInstance() = SplashFragment()
-
     }
 }
